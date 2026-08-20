1c425230c6bb18a6c9264a03a42c52cd532915ba
mr-chelyshkin <argent-keel-0z@icloud.com>
2026-08-20 09:15:03 +0400

    update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -13,12 +13,31 @@ function escapeHtml(value: string) {
     .replaceAll('>', '&gt;')
 }
 
+function serializeJsonForHtml(value: unknown) {
+  return JSON.stringify(value).replaceAll('<', '\\u003c')
+}
+
+const websiteStructuredData = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: siteContent.brand.name,
+  url: siteContent.meta.url,
+}
+
 const siteContentPlugin = {
   name: 'site-content',
   transformIndexHtml(html: string) {
     return html
+      .replaceAll('%SITE_URL%', escapeHtml(siteContent.meta.url))
+      .replaceAll('%SITE_NAME%', escapeHtml(siteContent.brand.name))
       .replaceAll('%SITE_TITLE%', escapeHtml(siteContent.meta.title))
       .replaceAll('%SITE_DESCRIPTION%', escapeHtml(siteContent.meta.description))
+      .replaceAll('%SITE_IMAGE_URL%', escapeHtml(siteContent.meta.imageUrl))
+      .replaceAll('%SITE_IMAGE_ALT%', escapeHtml(siteContent.meta.imageAlt))
+      .replaceAll(
+        '%SITE_STRUCTURED_DATA%',
+        serializeJsonForHtml(websiteStructuredData),
+      )
   },
 }
 
